1cff6f05e21546c1e94709be4c626dabafd65600
Mohammad Fathi <mimfapost@gmail.com>
2025-10-17 16:37:07 +0330

    Update bootstrap

--- global.php
+++ global.php
@@ -1,8 +1,8 @@
 <?php
-//$ASEQ = '[my-subdomain-name]'; 	// (Optional) The current subdomain sequence or leave null if this file is in the root directory
-$BASE = '.base';                    // (Optional) The base directory you want to inherit all properties except what you changed
-//$SEQUENCES_PATCH = []; 		    // (Optional) An array to apply your custom changes in \_::$Sequences
-                                    // newdirectory, newaseq; // Add new directory to the \_::$Sequences
-                                    // directory, newaseq; // Update directory in the \_::$Sequences
-                                    // directory, null; // Remove thw directory from the \_::$Sequences
+$BASE = '.base'; 		                    // (Optional) The base directory you want to inherit all properties except what you changed
+// $ASEQ = '[the-carrent-directory-name]'; 	// (Optional) The current subdomain sequence, or leave null if this file is in the root directory
+// $SEQUENCES_PATCH = []; 		            // (Optional) An array to apply your custom changes in \_::$Sequences
+                                            // newdirectory, newaseq; // Add new directory to the \_::$Sequences
+                                            // directory, newaseq; // Update directory in the \_::$Sequences
+                                            // directory, null; // Remove thw directory from the \_::$Sequences
 ?>
