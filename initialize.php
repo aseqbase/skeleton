2ef7a641361c7ec55feec0784b5f3f4a3c6696b4
Mohammad Fathi <mimfapost@gmail.com>
2025-12-30 17:28:38 +0330

    Simplifing Simplify initialization and consolidate configuration/information into Back and Front objects. Deprecate separate route/config/info files and introduce a unified initialize/customize flow.

--- initialize.php
+++ initialize.php
@@ -1,3 +1,13 @@
 <?php
 // Initialize codes
 // It will executes at the start of all sequence
+
+// To unset the default router sat at the bottom layers
+\_::$Router->On()->Reset();
+
+/**
+ * Use your routers by below formats
+ * \_::$Router->On("A Part Of Path?")->Default("Route Name");
+ * Or use a suitable handler for example
+ * \_::$Router->On()->Default(fn($router)=>deliver(\MiMFa\Library\Struct::Heading1("Hello World!")));
+ */
